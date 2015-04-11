739e0f719b60c830c017b136e9b9bbc2c3260442
John Rockefeller <jrockefeller1@gmail.com>
2015-04-11 19:33:34 -0400

    Chance of spawning mineral in.

---
+++ game/Assets/Scripts/OreRandom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OreRandom : MonoBehaviour {
+
+	public int chanceOfDisablePercent = 10;
+
+	void Start () {
+	
+		foreach (Transform child in transform) {
+			if (Random.Range(1, 101) <= chanceOfDisablePercent &&
+				child.gameObject.tag == "Mining-Ore") {
+					
+				child.gameObject.SetActive(false);
+				
+			}
+		}
+	
+	}
+	
+	void Update () {
+	
+	}
+}
