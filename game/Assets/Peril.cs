1b6b7f24add26aa44643b511a41bf553d8244e11
John Rockefeller <jrockefeller1@gmail.com>
2015-04-12 11:32:46 -0400

    Add Perils from Wyatt.

---
+++ game/Assets/Peril.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Peril : MonoBehaviour {
+	public float likelihood = 0.1f;
+	public GameObject dustStorm;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int peril = 0;
+		if (Random.value > 1.0f - likelihood) {
+			peril = Random.Range(0, 5);
+			if (peril == 0 && !dustStorm.activeSelf) {
+				Debug.LogError("Duststorm begins!");
+				dustStorm.SetActive(true);
+			}
+		}
+	}
+}
