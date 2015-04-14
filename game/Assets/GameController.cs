17dbc288bc1ad48929efd926ba411c290ee270fd
John Rockefeller <jrockefeller1@gmail.com>
2015-04-14 18:06:52 -0400

    Minor formatting.

--- game/Assets/GameController.cs
+++ game/Assets/GameController.cs
@@ -16,12 +16,8 @@ public class GameController : MonoBehaviour {
 	public bool CamToggleState = false;
 	Camera currentRobotCam;
 
-
-
 	Color laserColor1 = new Color(1, 0, 0, 0.5f);
-	Color laserColor2 = new Color(1, .17f, .17f, 0.4f);
 	LineRenderer lineRenderer;
-	//public Transform Laser;
 	public Material laserMat;
 	
 	//public Transform target;
@@ -30,14 +26,12 @@ public class GameController : MonoBehaviour {
 	void Start () {
 
 		
-		//Lasers
+		// Mining Lasers
 		lineRenderer = gameObject.AddComponent<LineRenderer>();
-		//lineRenderer.material = new Material (Shader.Find("Particles/Additive"));
 		lineRenderer.material = laserMat;
 		lineRenderer.SetColors(laserColor1, laserColor1);
 		lineRenderer.SetWidth(.3f,.1f);
 		lineRenderer.SetVertexCount(2);
-		//Lasers
 
 		for(int i = 0; i < Robots.Length; i++)
 		{
@@ -58,10 +52,6 @@ public class GameController : MonoBehaviour {
 			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) {
 
 				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-
-
-
-
 
 				Physics.Raycast (ray, out hitInfo, Mathf.Infinity);
 				
@@ -88,7 +78,6 @@ public class GameController : MonoBehaviour {
 				if ((hitInfo.collider.tag == "TubeEntrance"|| hitInfo.collider.tag == "Building") && currentRobot.GetComponent<Robot_surfaceMove>().inTube == false) {
 
 					if (currentRobot != null) {
-						//currentRobot.GetComponent<Robot_surfaceMove>().dest = hitInfo.collider.transform;
 						currentRobot.GetComponent<Robot_surfaceMove>().target = hitInfo.point;
 						currentRobot.GetComponent<Robot_surfaceMove>().moving = true;
 					}
@@ -100,33 +89,23 @@ public class GameController : MonoBehaviour {
 				if (hitInfo.collider.tag == "CaveFloor")	{
 
 					if (currentRobot != null) {
-						//currentRobot.GetComponent<Robot_surfaceMove>().dest = hitInfo.collider.transform;
 						currentRobot.GetComponent<Robot_surfaceMove>().target = hitInfo.point;
 						currentRobot.GetComponent<Robot_surfaceMove>().moving = true;
 					}
 					else {
 						DeselectRobot();
 					}
-					//moving = true;
-					//dest = hitInfo.collider.transform;
 					
 				} else if (hitInfo.collider.tag == "Mining-Ore") {
 					if (currentRobot != null) {
 
-
-
 						currentRobot.GetComponent<ResourceConverter>().online = true;
 
-
-
-
-						//Lasers
+						// Mining Laser
 						lineRenderer.enabled=true;
-						//lineRenderer.SetPosition(0, new Vector3(transform.position.x, transform.position.y - 2, transform.position.z));
 						lineRenderer.SetPosition(0, currentRobot.transform.position);
 						lineRenderer.SetPosition(1, hitInfo.point);
 						StartCoroutine(laser_die());
-
 					}
 				}
 				
@@ -137,14 +116,12 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
-	public void EnterTube(int tube, GameObject robot){
-			CamToggleButton.SetActive(true);
-			robot.transform.position = SpawnPoints[tube].transform.position;
-
-
+	public void EnterTube(int tube, GameObject robot) {
+		CamToggleButton.SetActive(true);
+		robot.transform.position = SpawnPoints[tube].transform.position;
 	}
 
-	public void SelectRobot (int num){
+	public void SelectRobot (int num) {
 		DeselectRobot ();
 		currentRobot = Robots[num];
 		if (currentRobot.GetComponent<Robot_surfaceMove>().inTube == true){
@@ -154,18 +131,19 @@ public class GameController : MonoBehaviour {
 		currentRobot.transform.GetChild(3).gameObject.SetActive(true);
 	}
 
-	public void DeselectRobot(){
+	public void DeselectRobot() {
 		if (currentRobot != null) {
-						currentRobot.transform.GetChild (1).gameObject.SetActive (false);
-						currentRobot.transform.GetChild (2).gameObject.SetActive (false);
-						currentRobot.transform.GetChild (3).gameObject.SetActive (false);
-						currentRobot = null;
-				}
+			currentRobot.transform.GetChild (1).gameObject.SetActive (false);
+			currentRobot.transform.GetChild (2).gameObject.SetActive (false);
+			currentRobot.transform.GetChild (3).gameObject.SetActive (false);
+			currentRobot = null;
+		}
 	}
 
-	public void CamToggler(){
-		Debug.Log ("CamToggler");
+	public void CamToggler() {
+		
 		CamToggleState = !CamToggleState;
+		
 		if (CamToggleState == true) {
 				currentRobot.transform.GetChild (2).gameObject.SetActive (true);
 				Surface_PIP.gameObject.SetActive(true);
