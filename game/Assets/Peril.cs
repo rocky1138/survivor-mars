733aa492bb47f7f7266f2bc9f5385d1d6bc5726a
rich <rich@mbp>
2015-04-18 16:15:10 -0400

    weather back in my scene, perils updated

--- game/Assets/Peril.cs
+++ game/Assets/Peril.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Peril : MonoBehaviour {
 	public float likelihood = 0.1f;
+	public float frequency = 10f;
 	public GameObject dustStorm;
 	public GameObject asteroid;
 	public AudioClip warning;
@@ -10,27 +11,29 @@ public class Peril : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		InvokeRepeating("Perils", frequency, frequency);
 	}
+
 	
 	// Update is called once per frame
-	void Update () {
+	void Perils () {
 		int peril = 0;
-		if (Random.value > 1.0f - likelihood) {
-			peril = Random.Range(0, 5);
-			if (peril == 0 && !dustStorm.activeSelf) {
-				Debug.Log("Duststorm begins!");
-				dustStorm.SetActive(true);
-				audio.PlayOneShot(warning);
+			if (Random.value > 1.0f - likelihood) {
+				peril = Random.Range(0, 5);
+				if (peril == 0 && !dustStorm.activeSelf) {
+					Debug.Log("Duststorm begins!");
+					dustStorm.SetActive(true);
+					audio.PlayOneShot(warning);
+				}
+				if (peril == 1 && !dustStorm.activeSelf) {
+					Debug.Log("Asteroid begins!");
+					//asteroid.SetActive(true);
+					audio.PlayOneShot(warning);
+					//Instantiate(asteroid);
+					Debug.Log("Asteroids");
+					GameObject target = targets[Random.Range(0, targets.Length)];
+					Instantiate(asteroid, new Vector3(target.transform.position.x, target.transform.position.y + 20, target.transform.position.z), transform.rotation);
+				}
 			}
-			if (peril == 1 && !dustStorm.activeSelf) {
-				Debug.Log("Asteroid begins!");
-				//asteroid.SetActive(true);
-				audio.PlayOneShot(warning);
-				//Instantiate(asteroid);
-				Debug.Log("Asteroids");
-				GameObject target = targets[Random.Range(0, targets.Length)];
-				Instantiate(asteroid, new Vector3(target.transform.position.x, target.transform.position.y + 20, target.transform.position.z), transform.rotation);
-			}
-		}
 	}
 }
