d26dfcd031c93b9fc422531e24580bab95ac3313
John Rockefeller <jrockefeller1@gmail.com>
2015-04-18 10:35:22 -0400

    Merge Richard's changes in.

---
+++ game/Assets/DustStorm.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DustStorm : MonoBehaviour {
+	public float duration = 5.0f;
+	float start;
+	float finish;
+
+	// Use this for initialization
+	void OnEnable () {
+		Debug.LogError("Starting Dust Storm..............");
+		start = Time.time;
+		finish = Time.time + duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > finish) {
+			gameObject.SetActive(false);
+			Debug.LogError("Duststorm ends!");
+		}
+	}
+}
---
+++ game/Assets/Explosion.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+
+	void OnCollisionEnter(Collision other){
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
