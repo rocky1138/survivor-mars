bc0a4429350b54e214d6a88c5641fa29c6d56b95
John Rockefeller <jrockefeller1@gmail.com>
2015-04-19 12:31:59 -0400

    Perils actually damage installations.

--- game/Assets/DustStorm.cs
+++ game/Assets/DustStorm.cs
@@ -2,10 +2,17 @@
 using System.Collections;
 
 public class DustStorm : MonoBehaviour {
+	
+	public GameObject buildingSettlement;
+	public GameObject buildingSolarArray;
+	public GameObject buildingAlgaeFarm;
+	
 	public float duration = 5.0f;
-	float start;
-	float finish;
+	public int damage = 1;
 
+	private float start;
+	private float finish;
+	
 	// Use this for initialization
 	void OnEnable () {
 		start = Time.time;
@@ -15,6 +22,9 @@ public class DustStorm : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > finish) {
+			buildingSettlement.GetComponent<BuildingManager>().damage(damage);
+			buildingSolarArray.GetComponent<BuildingManager>().damage(damage);
+			buildingAlgaeFarm.GetComponent<BuildingManager>().damage(damage);
 			gameObject.SetActive(false);
 		}
 	}
--- game/Assets/Peril.cs
+++ game/Assets/Peril.cs
@@ -9,6 +9,10 @@ public class Peril : MonoBehaviour {
 	public AudioClip warning;
 	public GameObject[] targets;
 	public GameObject GameController;
+	public int asteroidDamage = 30;
+	
+	private GameObject target;
+	
 	int warningNum;
 
 	// Use this for initialization
@@ -20,27 +24,37 @@ public class Peril : MonoBehaviour {
 	// Update is called once per frame
 	void Perils () {
 		int peril = 0;
-			if (Random.value > 1.0f - likelihood) {
-				peril = Random.Range(0, 5);
-				if (peril == 0 && !dustStorm.activeSelf) {
-					Debug.Log("Duststorm begins!");
-					dustStorm.SetActive(true);
-					audio.PlayOneShot(warning);
-					warningNum = 0;
-					GameController.GetComponent<ToastNotifications>().WarnNotifications(warningNum);
-				}
-				if (peril == 1 && !dustStorm.activeSelf) {
-					Debug.Log("Asteroid begins!");
-					//asteroid.SetActive(true);
-					audio.PlayOneShot(warning);
-					//Instantiate(asteroid);
-					Debug.Log("Asteroids");
-					GameObject target = targets[Random.Range(0, targets.Length)];
-					Instantiate(asteroid, new Vector3(target.transform.position.x, target.transform.position.y + 20, target.transform.position.z), transform.rotation);
-					warningNum = 1;
-					GameController.GetComponent<ToastNotifications>().WarnNotifications(warningNum);
-				}
-
+		if (Random.value > 1.0f - likelihood) {
+			peril = Random.Range(0, 5);
+			if (peril == 0 && !dustStorm.activeSelf) {
+				Debug.Log("Duststorm begins!");
+				dustStorm.SetActive(true);
+				audio.PlayOneShot(warning);
+				warningNum = 0;
+				GameController.GetComponent<ToastNotifications>().WarnNotifications(warningNum);
 			}
+			if (peril == 1 && !dustStorm.activeSelf) {
+				
+				Debug.Log("Asteroid begins!");
+				
+				audio.PlayOneShot(warning);
+				
+				target = targets[Random.Range(0, targets.Length - 1)];
+				
+				Debug.Log("Peril target: " + target);
+				
+				Instantiate(asteroid, new Vector3(target.transform.position.x, target.transform.position.y + 20, target.transform.position.z), transform.rotation);
+				
+				warningNum = 1;
+				
+				GameController.GetComponent<ToastNotifications>().WarnNotifications(warningNum);
+				
+				Invoke("AsteroidDamageTarget", 5.0f);
+			}
+		}
+	}
+	
+	void AsteroidDamageTarget () {
+		target.GetComponent<BuildingManager>().damage(asteroidDamage);
 	}
 }
